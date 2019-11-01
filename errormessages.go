5e9f48f4285a315bc2978308a648c3e3cfcf4429
bsoniam <soniam.bogos@gmail.com>
2019-11-01 17:03:21 +0100

    fixup! whitelist existing username or email error

--- errormessages.go
+++ errormessages.go
@@ -1,15 +1,16 @@
 package keycloak
 
 const (
-	MsgErrMissingParam          = "missingParameter"
-	MsgErrInvalidParam          = "invalidParameter"
-	MsgErrCannotObtain          = "cannotObtain"
-	MsgErrCannotMarshal         = "cannotMarshal"
-	MsgErrCannotUnmarshal       = "cannotUnmarshal"
-	MsgErrCannotParse           = "cannotParse"
-	MsgErrCannotCreate          = "cannotCreate"
-	MsgErrUnkownHTTPContentType = "unkownHTTPContentType"
-	MsgErrExistingValue         = "existing"
+	MsgErrMissingParam              = "missingParameter"
+	MsgErrInvalidParam              = "invalidParameter"
+	MsgErrCannotObtain              = "cannotObtain"
+	MsgErrCannotMarshal             = "cannotMarshal"
+	MsgErrCannotUnmarshal           = "cannotUnmarshal"
+	MsgErrCannotParse               = "cannotParse"
+	MsgErrCannotCreate              = "cannotCreate"
+	MsgErrUnkownHTTPContentType     = "unkownHTTPContentType"
+	MsgErrUnknownResponseStatusCode = "unknownResponseStatusCode"
+	MsgErrExistingValue             = "existing"
 
 	EvenParams       = "key/valParametersShouldBeEven"
 	TokenProviderURL = "tokenProviderURL"
