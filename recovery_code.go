80397ac336a938078a0a41c7e42aa3f08ef38595
harture <harture414@gmail.com>
2020-01-07 18:00:43 +0100

    Add recovery code api

---
+++ recovery_code.go
@@ -0,0 +1,20 @@
+package keycloak
+
+import "gopkg.in/h2non/gentleman.v2/plugins/url"
+
+const (
+	recoveryCodePath = "/auth/realms/:realm/recovery-code"
+)
+
+type RecoveryCodeRepresentation struct {
+	Code *string `json:"code,omitempty"`
+}
+
+// CreateRecoveryCode creates a new recovery code authenticator and returns the code.
+func (c *Client) CreateRecoveryCode(accessToken string, realmName string, userID string) (RecoveryCodeRepresentation, error) {
+	var resp = RecoveryCodeRepresentation{}
+
+	var plugins = append(createQueryPlugins("userid", userID), url.Path(recoveryCodePath), url.Param("realm", realmName))
+	_, err := c.post(accessToken, &resp, plugins...)
+	return resp, err
+}
