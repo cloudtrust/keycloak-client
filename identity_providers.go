d0d568598540d0b2471edb9d9ac9f45dd04b7322
Lorenceau Pablo <pablo.lorenceau@elca.ch>
2020-03-17 22:19:32 +0100

    add calls to identity providers and test

---
+++ identity_providers.go
@@ -0,0 +1,35 @@
+package keycloak
+
+import (
+	"errors"
+
+	"gopkg.in/h2non/gentleman.v2/plugins/url"
+)
+
+const (
+	idpsPath     = "/auth/admin/realms/:realm/identity-provider/instances"
+	idpAliasPath = "/auth/admin/realms/:realm/identity-provider/instances/:alias"
+)
+
+func (c *Client) GetIdps(accessToken string, realmName string, paramKV ...string) ([]IdentityProviderRepresentation, error) {
+	if len(paramKV)%2 != 0 {
+		return nil, errors.New(MsgErrInvalidParam + "." + EvenParams)
+	}
+
+	var resp = []IdentityProviderRepresentation{}
+	var plugins = append(createQueryPlugins(paramKV...), url.Path(idpsPath), url.Param("realm", realmName))
+	var err = c.get(accessToken, &resp, plugins...)
+	return resp, err
+}
+
+func (c *Client) GetTheIdp(accessToken string, realmName string, idpAlias string, paramKV ...string) (IdentityProviderRepresentation, error) {
+	var resp = IdentityProviderRepresentation{}
+
+	if len(paramKV)%2 != 0 {
+		return resp, errors.New(MsgErrInvalidParam + "." + EvenParams)
+	}
+
+	var plugins = append(createQueryPlugins(paramKV...), url.Path(idpAliasPath), url.Param("realm", realmName), url.Param("alias", idpAlias))
+	var err = c.get(accessToken, &resp, plugins...)
+	return resp, err
+}
---
+++ identity_providers_test.go
@@ -0,0 +1,58 @@
+package keycloak
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const (
+	kcHost   = "localhost:8080"
+	master   = "master"
+	realm    = "identity-broker-realm"
+	idpAlias = "keycloak-oidc"
+	username = "admin"
+	password = "admin"
+)
+
+func makeDefaultClient() (*Client, error) {
+	timeout, _ := time.ParseDuration("10s")
+	config := Config{
+		AddrTokenProvider: kcHost,
+		AddrAPI:           kcHost,
+		Timeout:           timeout,
+	}
+
+	return New(config)
+}
+
+func TestGetTheIdp(t *testing.T) {
+	client, err := makeDefaultClient()
+	assert.Nil(t, err)
+
+	token, err := client.GetToken(master, username, password)
+	fmt.Printf(token)
+	assert.Nil(t, err)
+
+	idp, err := client.GetTheIdp(token, realm, idpAlias)
+	assert.Nil(t, err)
+	assert.Equal(t, idpAlias, *(idp.Alias))
+
+}
+
+func TestGetIdps(t *testing.T) {
+	client, err := makeDefaultClient()
+	assert.Nil(t, err)
+
+	token, err := client.GetToken(master, username, password)
+	fmt.Printf(token)
+	assert.Nil(t, err)
+
+	idps, err := client.GetIdps(token, realm)
+	assert.Nil(t, err)
+	assert.Equal(t, 1, len(idps))
+	assert.Equal(t, idpAlias, *(idps[0].Alias))
+
+}
