a65d20c126388b5409ecedb397d0eba80301f374
Beuchat Bastien <bastien.beuchat@elca.ch>
2021-11-17 11:21:28 +0100

    [CLOUDTRUST-3881] FIX CI Migrate from dep to go modules

--- toolbox/mock_test.go
+++ toolbox/mock_test.go
@@ -2,4 +2,4 @@ package toolbox
 
 import _ "github.com/golang/mock/mockgen/model"
 
-//go:generate mockgen -destination=./mock/logger.go -package=mock -mock_names=Logger=Logger github.com/cloudtrust/keycloak-client/toolbox Logger
+//go:generate mockgen --build_flags=--mod=mod -destination=./mock/logger.go -package=mock -mock_names=Logger=Logger github.com/cloudtrust/keycloak-client/toolbox Logger
