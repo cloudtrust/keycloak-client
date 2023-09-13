6c6bbb99e7b723b25aa4fd3e3f2ab45e1eecf514
Claire Lefrancq <claire.lefrancq@elca.ch>
2023-09-13 14:24:15 +0200

    [EDKEDULOG-392] Modules for IDPs

--- api/identity_providers.go
+++ api/identity_providers.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"github.com/cloudtrust/keycloak-client/v2"
+	"gopkg.in/h2non/gentleman.v2/plugins/body"
 	"gopkg.in/h2non/gentleman.v2/plugins/url"
 )
 
@@ -16,6 +17,11 @@ func (c *Client) GetIdps(accessToken string, realmName string) ([]keycloak.Ident
 	var resp = []keycloak.IdentityProviderRepresentation{}
 	var err = c.get(accessToken, &resp, url.Path(idpsPath), url.Param("realm", realmName))
 	return resp, err
+}
+
+// UpdateIdp updates the identity provider. idpAlias is the alias of identity provider.
+func (c *Client) UpdateIdp(accessToken string, realmName, idpAlias string, idpRep keycloak.IdentityProviderRepresentation) error {
+	return c.put(accessToken, url.Path(idpAliasPath), url.Param("realm", realmName), url.Param("alias", idpAlias), body.JSON(idpRep))
 }
 
 // GetIdp gets an identity provider matching the given alias
