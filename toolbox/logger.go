d4fe0812b017f6c2877eb710e3760658b2d2d7d5
Romain Poiffaut <romain.poiffaut@elca.ch>
2024-02-22 18:46:48 +0100

    DEBUG 401Issue

--- toolbox/logger.go
+++ toolbox/logger.go
@@ -5,4 +5,5 @@ import "context"
 // Logger interface for logging with level
 type Logger interface {
 	Warn(ctx context.Context, keyvals ...interface{})
+	Info(ctx context.Context, keyvals ...interface{})
 }
