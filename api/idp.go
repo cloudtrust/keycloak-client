4d35ceef332793c20137aa974252b8f90e5da319
Kilian Schneiter <kilian.schneiter@elca.ch>
2026-06-18 16:49:38 +0200

    Rename IDP team member path

--- api/idp.go
+++ api/idp.go
@@ -3,13 +3,13 @@ package api
 import "gopkg.in/h2non/gentleman.v2/plugins/url"
 
 const (
-	kcIDPTeamMemberPath = "/auth/realms/:realmReq/api/idp/realms/:realm/team-members/:userId"
+	ctIDPTeamMemberPath = "/auth/realms/:realmReq/api/idp/realms/:realm/team-members/:userId"
 )
 
 // DeleteExtIDPTeamMemberUser deletes a team member user
 func (c *Client) DeleteExtIDPTeamMemberUser(accessToken string, realmReqName string, realmName string, userID string) error {
 	return c.forRealm(accessToken, realmName).
-		delete(accessToken, url.Path(kcIDPTeamMemberPath),
+		delete(accessToken, url.Path(ctIDPTeamMemberPath),
 			url.Param("realmReq", realmReqName),
 			url.Param("realm", realmName),
 			url.Param("userId", userID))
